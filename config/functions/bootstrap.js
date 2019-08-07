2c1cac936724664c9b933b11414dfb3cee0fe2e1
Maximus <maximgenov@abv.bg>
2019-08-07 18:16:52 +0300

    Load .env file.

--- config/functions/bootstrap.js
+++ config/functions/bootstrap.js
@@ -1,4 +1,4 @@
-'use strict';
+'use strict'
 
 /**
  * An asynchronous bootstrap function that runs before
@@ -7,6 +7,13 @@
  * This gives you an opportunity to set up your data model,
  * run jobs, or perform some special logic.
  */
+const dotenv = require('dotenv');
+
+if (process.env.NODE_ENV === 'production') {
+  dotenv.config({ path: './.env.production' });
+} else {
+  dotenv.config({ path: './.env.development' });
+}
 
 module.exports = cb => {
   cb();
